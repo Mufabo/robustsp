7d365a06072c8234907037359bff8e1874f3a205
Fatih <fatih.bostanci@hotmail.de>
2019-06-29 17:36:30 +0200

    radardetector

---
+++ robustsp/Covariance/signcm.py
@@ -0,0 +1,30 @@
+"""
+calculates the spatial sign covariance matrix (SCM). 
+
+INPUT 
+        X: Numeric data matrix of size N x p. Each row represents one 
+          observation, and each column represents one variable.
+   center: logical (true/false). If true, then center the data using
+           spatial median. Default is false
+OUTPUT: 
+spatial sign covariance matrix
+and spatial median (computed only if center = true)
+"""
+import robustsp as rsp
+import numpy as np
+
+def signcm(Xx,center=False,EPS=1e-6,):
+    X = np.array(Xx)
+    
+    if center: 
+        smed0 = rsp.spatmed(X)
+        X = X - smed0[:,None]
+    else: smed0 = []
+        
+    len = np.sqrt(np.sum(X*np.conj(X),axis=1))
+    X[len != 0,:] = X
+    len[len!=0] = len
+    n,p = X.shape
+    len[len<EPS]=EPS
+    X = X / len[:,None]
+    return X.T @ np.conj(X)/n , smed0
