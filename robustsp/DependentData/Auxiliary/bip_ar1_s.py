bb6698742b09ee3b4a5ed2cdc7b1232f84774e3c
Fatih <fatih.bostanci@hotmail.de>
2019-10-04 11:57:19 +0200

    ar_est_bip_s debugging

---
+++ robustsp/DependentData/Auxiliary/bip_ar1_s.py
@@ -0,0 +1,64 @@
+import numpy as np
+import robustsp as rsp
+
+def bip_ar1_s(x, N, phi_grid, fine_grid,kap2):
+
+    a_scale_final = rsp.m_scale(x) # AR(0): residual scale equals observation scale
+    
+    # grid search for partial autocorrelations
+    a_bip_sc = np.zeros(len(phi_grid))
+    a_sc = np.zeros(len(phi_grid))
+    for mm in range(len(phi_grid)):
+        a = np.zeros(len(x)) # residuals for BIP-AR
+        a2= np.zeros(len(x)) # residuals for AR
+        
+        lambd = rsp.ma_infinity(phi_grid[mm], 0, 100)
+        sigma_hat = a_scale_final/np.sqrt(1+kap2*np.sum(lambd**2)) # sigma used for BIP-model
+        for ii in range(1,N):
+            a[ii] = x[ii]-phi_grid[mm]*\
+            (x[ii-1]-a[ii-1]+sigma_hat*rsp.eta(a[ii-1]/sigma_hat)) # residuals for BIP-AR
+            a2[ii] = x[ii] - phi_grid[mm]*x[ii-1] # residuals for AR
+             
+        a_bip_sc[mm] = rsp.m_scale(a[1:]) # tau-scale of residuals for BIP-AR
+        a_sc[mm] = rsp.m_scale(a2[1:]) # tau-scale of residuals for AR
+        
+    poly_approx = np.polyfit(phi_grid, a_bip_sc, 5) # polynomial approximation of tau scale objective function for BIP-AR(1) tau-estimates
+    a_interp_scale = np.polyval(poly_approx,fine_grid) # interpolation of tau scale objective function for BIP-AR(1) tau-estimates to fine grid
+    poly_approx2 = np.polyfit(phi_grid,a_sc,5) # polynomial approximation of  tau scale objective function for AR(1) tau-estimates
+    a_interp_scale2 = np.polyval(poly_approx2, fine_grid)
+    
+    temp = np.min(a_interp_scale)
+    ind_max = np.argmin(a_interp_scale)
+    phi = fine_grid[ind_max] # tau-estimate unter the BIP-AR(1)
+    temp2 = np.min(a_interp_scale2)
+    ind_max2 = np.argmin(a_interp_scale2)
+    phi2 = fine_grid[ind_max2] # tau-estimate under the AR(1)
+    
+    # final estimate maximizes robust likelihood of the two
+    if temp2<temp:
+        phi_s = phi2
+        temp = temp2
+    else:
+        phi_s = phi
+        
+    phi_hat = phi_s # final BIP-tau-estimate for AR(1)
+    
+    # final AR(1) tau-scale-estimate depending on phi_hat
+    lambd = rsp.ma_infinity(phi_hat, 0, 100)
+    sigma_hat = a_scale_final/np.sqrt(1+kap2*np.sum(lambd**2))
+    a = np.zeros(len(x)) # residuals for BIP-AR
+    a2= np.zeros(len(x)) # residuals for AR
+    
+    x_filt = np.zeros(len(x))
+    
+    for ii in range(1,N):
+        a[ii] = x[ii]-phi_hat*(x[ii-1]-a[ii-1]+sigma_hat*rsp.eta(a[ii-1]/sigma_hat))
+        a2[ii] = x[ii] - phi_hat*x[ii-1]
+        x_filt[ii] = x[ii] - a[ii] + sigma_hat*rsp.eta(a[ii]/sigma_hat)
+        
+    if temp2<temp:
+        a_scale_final = [a_scale_final, rsp.m_scale(a[1:])]
+    else:
+        a_scale_final = [a_scale_final, rsp.m_scale(a2[1:])]
+        
+    return x_filt, phi_hat, a_scale_final
--- robustsp/__init__.py
+++ robustsp/__init__.py
@@ -43,6 +43,7 @@ from robustsp.Covariance.Mscat import Mscat
 from robustsp.DependentData.Auxiliary.muler_rho1 import muler_rho1
 from robustsp.DependentData.Auxiliary.m_scale import m_scale
 from robustsp.DependentData.Auxiliary.ma_infinity import ma_infinity
+from robustsp.DependentData.Auxiliary.bip_ar1_s import bip_ar1_s
 
 from robustsp.DependentData.Auxiliary.eta import eta
 
