9cd98939951c2510f822e63a47b6aa946af23b6c
M. Fatih B <m.fatih.bostanci@gmail.com>
2021-06-02 10:05:29 +0200

    cleanup the second

--- robustsp/AuxiliaryFunctions/.ipynb_checkpoints/psihub-checkpoint.py
+++
@@ -1,9 +0,0 @@
-'''
-Huber's score function psi: input is N  data vector x which can be complex
-or real and threshold contant c
-'''
-import numpy as np
-
-def psihub(x,c):
-    return (x * (np.abs(x)<=c) + c*np.sign(x) *(np.abs(x)>c))
-    
